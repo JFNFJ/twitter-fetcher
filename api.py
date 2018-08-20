23645645b45910eb625447b07ba3ba98599afc77
Federico Bonisconti <federico.bonisconti@mercadolibre.com>
2018-08-20 02:11:21 -0300

    added sign in

--- api.py
+++ api.py
@@ -2,14 +2,18 @@ import os
 import json
 import signal
 import datetime
+import jwt
 from flask import request
 from multiprocessing import Process
 from TwitterFetcher import TwitterFetcher
 from settings import app
 from oauth import default_provider
 from models.models import User
+from flask_sqlalchemy import SQLAlchemy
 
 oauth = default_provider(app)
+db = SQLAlchemy(app)
+EXPIRATION_HOURS = 24
 
 
 @app.route("/start_thread", methods=['POST'])
@@ -35,7 +39,7 @@ def finish():
 def sign_up():
     req = request.get_json(force=True)
     app.logger.debug("Request: %s", req)
-    user = User.create_user(req)
+    user = User.create_user(req['name'], req['email'], req['password'])
     app.logger.debug("User: %s", user)
     return str(f"Sign up {req} {url}")
 
@@ -44,8 +48,20 @@ def sign_up():
 def login():
     req = request.get_json(force=True)
     app.logger.debug("Request: %s", req)
-    # TODO
-    return str("Login")
+    name = req["name"]
+    if not name:
+        return json.dumps({'error': 'Nombre de usuario o email', 'code': 400}), 400
+    password = req["password"]
+    if not password:
+        return json.dumps({'error': 'Una clave debe ser provista', 'code': 400}), 400
+    user = User.query.filter_by(name=name).first()
+    if not user or not user.validate_password(password):
+        return json.dumps({'error': 'Usuario o clave incorrectos', 'code': 400}), 400
+
+    expiration_date = datetime.datetime.utcnow() + datetime.timedelta(hours=EXPIRATION_HOURS)
+    token = jwt.encode({'name': user.name, 'exp': expiration_date}, app.secret_key, algorithm='HS256')
+
+    return json.dumps({'name': name, 'token': token.decode('utf-8')}), 200
 
 
 @app.route("/log_out", methods=['POST'])
