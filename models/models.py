cf6842917b83c80f40d6167e2d6e85fd7fcc0ef6
Federico Bonisconti <federico.bonisconti@mercadolibre.com>
2018-09-14 17:53:16 -0300

    reverted models changes

--- models/models.py
+++ models/models.py
@@ -60,7 +60,6 @@ class Topic(db.Model):
     @staticmethod
     def create(user_id, name, deadline):
         topic = Topic(user_id=user_id, name=name, deadline=deadline)
-        topic.general_result = GeneralResult()
         db.session.add(topic)
         db.session.commit()
         return topic
@@ -115,3 +114,101 @@ class LocationResult(db.Model):
                f"negative='{self.negative}', neutral='{self.neutral}', location='{self.location}')>"
 
 
+# OAuth Models
+
+
+class Client(db.Model):
+    name = db.Column(db.String(40))
+    description = db.Column(db.String(400))
+    user_id = db.Column(db.ForeignKey('users.id'))
+    user = db.relationship('User')
+
+    client_id = db.Column(db.String(40), primary_key=True)
+    client_secret = db.Column(db.String(55), unique=True, index=True,
+                              nullable=False)
+
+    is_confidential = db.Column(db.Boolean)
+    _redirect_uris = db.Column(db.Text)
+    _default_scopes = db.Column(db.Text)
+
+    @property
+    def client_type(self):
+        if self.is_confidential:
+            return 'confidential'
+        return 'public'
+
+    @property
+    def redirect_uris(self):
+        if self._redirect_uris:
+            return self._redirect_uris.split()
+        return []
+
+    @property
+    def default_redirect_uri(self):
+        return self.redirect_uris[0]
+
+    @property
+    def default_scopes(self):
+        if self._default_scopes:
+            return self._default_scopes.split()
+        return []
+
+
+class Grant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id', ondelete='CASCADE')
+    )
+    user = db.relationship('User')
+
+    client_id = db.Column(
+        db.String(40), db.ForeignKey('client.client_id'),
+        nullable=False,
+    )
+    client = db.relationship('Client')
+    code = db.Column(db.String(255), index=True, nullable=False)
+    redirect_uri = db.Column(db.String(255))
+    expires = db.Column(db.DateTime)
+    _scopes = db.Column(db.Text)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    @property
+    def scopes(self):
+        if self._scopes:
+            return self._scopes.split()
+        return []
+
+
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(
+        db.String(40), db.ForeignKey('client.client_id'),
+        nullable=False,
+    )
+    client = db.relationship('Client')
+
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id')
+    )
+    user = db.relationship('User')
+    token_type = db.Column(db.String(40))
+    access_token = db.Column(db.String(255), unique=True)
+    refresh_token = db.Column(db.String(255), unique=True)
+    expires = db.Column(db.DateTime)
+    _scopes = db.Column(db.Text)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    @property
+    def scopes(self):
+        if self._scopes:
+            return self._scopes.split()
+        return []
