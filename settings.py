4f911386bd827fea52975e2eccbff12a8db2116f
Julieta Colombo <julietanataliacolombo@yahoo.com.ar>
2018-09-16 22:08:50 -0300

    Configure mailer

--- settings.py
+++ settings.py
@@ -27,3 +27,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     f'postgresql+psycopg2://{POSTGRESQL_USER}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_HOST}/{POSTGRESQL_DB}'
 app.secret_key = os.getenv("SECRET_KEY")
+
+app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.getenv("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS")
+app.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL")
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
